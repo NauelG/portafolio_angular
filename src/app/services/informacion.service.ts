0bfe678bb02c62e41cdc0cfed25f713e337c9f8a
Nauel Gamero <g.nauel@gmail.com>
2018-02-26 23:02:31 +0100

    Servicio desde Firebase

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,14 +6,32 @@ export class InformacionService {
 
   info:any = {};
   cargada:boolean = false;
+  cargada_sobre_nosotros:boolean = false;
+
+  equipo:any[] = [];
 
   constructor( public http:Http ) {
+    this.carga_info();
+    this.carga_sobre_nosotros();
 
-    this.http.get("assets/data/info.pagina.json")
-      .subscribe( data =>{
-        console.log(data.json());
-        this.info = data.json();
-        this.cargada = true;
+   }
+
+   public carga_info(){
+     this.http.get("assets/data/info.pagina.json")
+       .subscribe( data =>{
+         this.info = data.json();
+         this.cargada = true;
+       })
+
+   }
+
+   public carga_sobre_nosotros(){
+     this.http.get("https://paginaweb-78b09.firebaseio.com/equipo.json")
+      .subscribe( data=>{
+        this.equipo = data.json();
+        console.log(this.equipo);
+        this.cargada_sobre_nosotros = true;
+
       })
    }
 
