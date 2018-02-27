e74ba9a8d89507c72e85a20dde816cb06d469e53
Nauel Gamero <g.nauel@gmail.com>
2018-02-27 21:54:11 +0100

    Agregamos productos desde firebase

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -22,6 +22,7 @@ public cargar_productos(){
     this.http.get("https://paginaweb-78b09.firebaseio.com/productos_idx.json")
       .subscribe( res =>{
         console.log(res.json());
+        this.productos = res.json();
         this.cargando = false;
       });
   }
